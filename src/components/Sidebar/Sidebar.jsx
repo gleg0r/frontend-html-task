29f329a3a07a46246fc3866a10a62ac53188a07b
gleg0r <egor103230@gmail.com>
2024-02-21 22:34:10 +0300

    done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import './sidebar.scss';
+import s from './sidebar.module.scss';
 import React from 'react';
-import classnames from 'classnames';
+import cn from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../assets/logo.png'
 
@@ -24,6 +24,8 @@ export default class Sidebar extends React.Component {
 
         this.state = {
             isOpened: true,
+            activeMainIndex: null,
+            activeFooterIndex: null,
         };
     }
 
@@ -35,43 +37,61 @@ export default class Sidebar extends React.Component {
         console.log(`going to "${path}"`);
     };
 
+    toggleSidebarMainItem = (index) => {
+        this.setState({activeMainIndex: index});
+    }
+
+    toggleSidebarFooterItem = (index) => {
+        this.setState({activeFooterIndex: index});
+    }
+
     render() {
-        const { isOpened } = this.state;
-        const containerClassnames = classnames('sidebar', { opened: isOpened });
+        const { isOpened, activeMainIndex, activeFooterIndex } = this.state;
+        const containerClassnames = cn(isOpened ? s.sidebar : s.sidebar__closed);
 
         return (
             <div className={ containerClassnames }>
-                <div>
+                <div className={s.sidebar__header}>
                     <img
+                        className={s.sidebar__img}
                         src={ logo }
                         alt="TensorFlow logo"
                     />
-                    <span>TensorFlow</span>
-                    <button onClick={ this.toggleSidebar }>
+                    <span className={cn(isOpened ? s.sidebar__title : s.sidebar__title_closed)}>TensorFlow</span>
+                    <button className={isOpened ? s.sidebar__btn : s.sidebar__btn_closed} onClick={ this.toggleSidebar }>
                         <FontAwesomeIcon icon={ isOpened ? 'angle-left' : 'angle-right' } />
                     </button>
                 </div>
+                <div className={s.sidebar__items}>
+                    <div className={s.sidebar__main}>
+                        {
+                            routes.map((route, index) => (
+                                <div
+                                    className={cn(activeMainIndex !== index ? s.sidebar__item : s.sidebar__item_active)}
+                                    key={ route.title }
+                                    onClick={ () => {this.goToRoute(route.path); this.toggleSidebarMainItem(index)}}
+                                >
+                                    <FontAwesomeIcon icon={ route.icon } />
+                                    <span className={cn(isOpened ? s.sidebar__textMain : s.sidebar__textClosed)}>{ route.title }</span>
+                                </div>
+                            ))
+                        }
+                    </div>
 
-                <div>
-                    {
-                        routes.map((route) => (
-                            <div key={ route.title } onClick={ () => this.goToRoute(route.path) }>
-                                <FontAwesomeIcon icon={ route.icon } />
-                                <span>{ route.title }</span>
-                            </div>
-                        ))
-                    }
-                </div>
-
-                <div>
-                    {
-                        bottomRoutes.map((route) => (
-                            <div key={ route.title } onClick={ () => this.goToRoute(route.path) }>
-                                <FontAwesomeIcon icon={ route.icon } />
-                                <span>{ route.title }</span>
-                            </div>
-                        ))
-                    }
+                    <div className={s.sidebar__footer}>
+                        {
+                            bottomRoutes.map((route, index) => (
+                                <div
+                                    className={cn(activeFooterIndex !== index ? s.sidebar__item : s.sidebar__item_active)}
+                                    key={ route.title }
+                                    onClick={ () => { this.goToRoute(route.path); this.toggleSidebarFooterItem(index) }}
+                                >
+                                    <FontAwesomeIcon icon={ route.icon } />
+                                    <span className={cn(isOpened ? s.sidebar__textFooter : s.sidebar__textClosed)}>{ route.title }</span>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         );
